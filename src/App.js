19e6f7d3db3493cd63cea5f2fb5acec349677a92
tareHej <128603982+tareHej@users.noreply.github.com>
2024-05-17 11:56:59 +0200

    försökt att fixa repo och subdomain för flygtracker

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Map from './components/Map'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Flight Tracker</h1>
+      <Map />
+    </div>
+  );
+};
+
+export default App;
