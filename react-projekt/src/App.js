b5b5274226978b75479f1f1c0893dc4c4cd39251
OQProjects <quijada.oswaldo@hotmail.com>
2024-05-20 12:01:23 +0200

    Småfix i GUI

--- react-projekt/src/App.js
+++ react-projekt/src/App.js
@@ -1,11 +1,35 @@
+import React from 'react';
+import Header from './components/Header/header';
+import Sidebar from './components/Sidebar/sidebar';
+import MapView from './components/MapView/mapview';
+import Footer from './components/Footer/footer';
+import './app.css';
+import { ThemeProvider, useTheme } from './ThemeContext';
 
-import './index.css';
+function AppContent() {
+  const { theme } = useTheme();
+
+  React.useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <div className={`app ${theme}`}>
+      <Header />
+      <div className="main-content">
+        <Sidebar />
+        <MapView />
+      </div>
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-     <h1>Welcome to the Flight Tracking app</h1>
-    </div>
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   );
 }
 
--- react-projekt/src/components/Header/header.js
+++ react-projekt/src/components/Header/header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './header.css';
+import { useTheme } from '../../ThemeContext';
 
 const Header = () => {
+  const { toggleTheme } = useTheme();
+
   return (
     <header className="header">
-      <div className="logo">Logo</div>
+      <div className="logo">
+        <img src="/logo.png" alt="Logo" />
+        <span>FlightTracker</span>
+      </div>
       <input type="text" placeholder="Search..." className="search-bar" />
-      <button className="theme-toggle">Theme Toggle</button>
+      <button className="theme-toggle" onClick={toggleTheme}>Theme Toggle</button>
     </header>
   );
 };
